1dd18c97bcdf21f761b37ce3f8c404359dcff560
Mark Sanders <mark.something@gmail.com>
2017-05-28 00:52:53 +0200

    That first version though!

---
+++ README.md
@@ -0,0 +1,5 @@
+# Noxys Extra Settings Info
+
+A Factorio modification that adds a small line to all settings descriptions indicating what the limits and defaults are for that setting.
+
+[Factorio mod portal page.](https://mods.factorio.com/mods/CobaltSky/Noxys_Extra_Settings_Info)
---
+++ settings-final-fixes.lua
@@ -0,0 +1,36 @@
+for k,v in pairs(data.raw["bool-setting"]) do
+	if v.default_value then
+		v.localised_description = {"noxys-extrasettingsinfo.merge", {"mod-setting-description." .. v.name}, "\n\n" .. "Default: " .. tostring(v.default_value) .. "."}
+	end
+end
+for _,s in pairs{"int-setting", "double-setting"} do
+	for k,v in pairs(data.raw[s]) do
+		local t = {}
+		if v.minimum_value then
+			table.insert(t, "Min: " .. v.minimum_value)
+		end
+		if v.default_value then
+			table.insert(t, "Default: " .. v.default_value)
+		end
+		if v.maximum_value then
+			table.insert(t, "Max: " .. v.maximum_value)
+		end
+		if #t then
+			v.localised_description = {"noxys-extrasettingsinfo.merge", {"mod-setting-description." .. v.name}, "\n\n" .. table.concat(t, " ") .. "."}
+		end
+	end
+end
+for k,v in pairs(data.raw["string-setting"]) do
+	local t = {}
+	if v.default_value then
+		table.insert(t, "Default: \"" .. v.default_value .. "\"")
+	end
+	if v.allow_blank == true then
+		table.insert(t, "Allows blank")
+	elseif v.allow_blank == false then
+		table.insert(t, "Disallows blank")
+	end
+	if #t then
+		v.localised_description = {"noxys-extrasettingsinfo.merge", {"mod-setting-description." .. v.name}, "\n\n" .. table.concat(t, ", ") .. "."}
+	end
+end
