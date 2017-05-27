89287000cbde53a348cf558cfef8a51b63eecb2e
Mark Sanders <mark.something@gmail.com>
2017-05-28 01:41:54 +0200

    Fixed a bug where "false" defaults would not show up.

--- settings-final-fixes.lua
+++ settings-final-fixes.lua
@@ -1,18 +1,18 @@
 for k,v in pairs(data.raw["bool-setting"]) do
-	if v.default_value then
+	if v.default_value ~= nil then
 		v.localised_description = {"noxys-extrasettingsinfo.merge", {"mod-setting-description." .. v.name}, "\n\n" .. "Default: " .. tostring(v.default_value) .. "."}
 	end
 end
 for _,s in pairs{"int-setting", "double-setting"} do
 	for k,v in pairs(data.raw[s]) do
 		local t = {}
-		if v.minimum_value then
+		if v.minimum_value ~= nil  then
 			table.insert(t, "Min: " .. v.minimum_value)
 		end
-		if v.default_value then
+		if v.default_value ~= nil  then
 			table.insert(t, "Default: " .. v.default_value)
 		end
-		if v.maximum_value then
+		if v.maximum_value ~= nil  then
 			table.insert(t, "Max: " .. v.maximum_value)
 		end
 		if #t then
@@ -22,7 +22,7 @@ for _,s in pairs{"int-setting", "double-setting"} do
 end
 for k,v in pairs(data.raw["string-setting"]) do
 	local t = {}
-	if v.default_value then
+	if v.default_value ~= nil  then
 		table.insert(t, "Default: \"" .. v.default_value .. "\"")
 	end
 	if v.allow_blank == true then
